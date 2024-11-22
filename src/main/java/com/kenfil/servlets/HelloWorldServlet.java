fd860182f3b1d184c013725ebf8cbb5918594c98
bumi0206 <bumitoshi@gmail.com>
2024-11-22 16:35:16 +0800

    Servlet update

--- src/main/java/com/kenfil/servlets/HelloWorldServlet.java
+++ src/main/java/com/kenfil/servlets/HelloWorldServlet.java
@@ -13,7 +13,8 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
+    String key1 = request.getParameter("key1");
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello world!" + key1 + "</h1>");
   }
 }
